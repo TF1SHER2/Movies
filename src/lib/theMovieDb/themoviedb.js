b05674f74e6b168dedea9113b71d3f6523395fb2
Thomas Fisher <tfisher2@crimson.ua.edu>
2021-08-25 20:54:10 -0500

    adding https to api reqs

--- src/lib/theMovieDb/themoviedb.js
+++ src/lib/theMovieDb/themoviedb.js
@@ -11,8 +11,8 @@ var theMovieDb = {};
 
 theMovieDb.common = {
   api_key: "1166b0545e86dbf82dc932fae3331f32",
-  base_uri: "http://api.themoviedb.org/3/",
-  images_uri: "http://image.tmdb.org/t/p/",
+  base_uri: "https://api.themoviedb.org/3/",
+  images_uri: "https://image.tmdb.org/t/p/",
   timeout: 5000,
   language: "en-US",
   generateQuery: function(options) {
