a669f1edd2c197aec8d3b43f793b1c400b3fc13d
Thomas Fisher <tfisher2@crimson.ua.edu>
2021-08-25 20:18:46 -0500

    testing routing

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,15 @@ function App() {
     <div className="App">
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Switch>
-          <Route exact path="/">
+          <Route exact path={process.env.PUBLIC_URL + "/"}>
             <Home />
           </Route>
-          <Route exact path="/moviesList">
+          <Route exact path={process.env.PUBLIC_URL + "/moviesList"}>
             <MoviesList
               type="popular"
             />
           </Route>
-          <Route exact path="/movie/:id">
+          <Route exact path={process.env.PUBLIC_URL + "/movie/:id"}>
             <MovieDetail />
           </Route>
         </Switch>
